97e9a3dd26dfaa50508df48117c784e6f92af790
Jakedalus <jacquedalus@gmail.com>
2020-05-15 15:46:40 -0400

    Add menu, About page, Services page

---
+++ src/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About us</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus quos
+        blanditiis mollitia optio quia qui recusandae, consectetur vitae et
+        pariatur rerum incidunt! Similique amet non tempore velit, minus
+        veritatis voluptatem?
+      </p>
+    </Layout>
+  )
+}
+
+export default AboutPage
---
+++ src/pages/services.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const ServicesPage = () => {
+  return (
+    <Layout>
+      <h1>Our Services</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Natus quos
+        blanditiis mollitia optio quia qui recusandae, consectetur vitae et
+        pariatur rerum incidunt! Similique amet non tempore velit, minus
+        veritatis voluptatem? Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Eum magni veniam exercitationem dicta omnis amet,
+        nostrum nisi, deserunt vero esse obcaecati dolor rerum minus earum
+        tempora at eveniet nihil est.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non nulla
+        pariatur numquam? Ipsam, quos? Esse, corporis, laudantium ullam nesciunt
+        amet cupiditate velit et, perferendis vitae magnam quis. Eaque, eos rem.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste,
+        voluptatibus? Eius architecto commodi sapiente voluptatum laboriosam
+        fuga incidunt. Excepturi numquam repellat natus nulla? Nemo expedita
+        libero quasi optio quos totam!
+      </p>
+    </Layout>
+  )
+}
+
+export default ServicesPage
